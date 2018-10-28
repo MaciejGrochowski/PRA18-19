3ff4c242453b996a46b6da638f9116f6b49a8468
Chomik <Szynka@Ser.pl>
2018-10-28 20:32:24 +0100

    Project v 1.0

--- PracowaniaProgramowania/src/main/java/quartz/job/SimpleJob.java
+++ PracowaniaProgramowania/src/main/java/quartz/job/SimpleJob.java
@@ -1,24 +1,217 @@
 package quartz.job;
-
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.time.temporal.ChronoUnit;
+import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Date;
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.api.set.SetIterable;
+import org.eclipse.collections.impl.multimap.bag.HashBagMultimap;
+import java.util.*;
 
-//Simplest job is a class that implements Job interface (execute method)
 public class SimpleJob implements org.quartz.Job {
 
-    int i = 0;
+    //False = Break; dayOfWeek=0 -> it's sunday
+    public static final boolean checkBreak (int dayOfWeek, LocalTime now){
+        LocalTime[] zaj = new LocalTime[6];
+        LocalTime[] prz = new LocalTime[6];
+        zaj[0] = LocalTime.of(8, 15);
+        zaj[1] = LocalTime.of(10, 00);
+        zaj[2] = LocalTime.of(11, 45);
+        zaj[3] = LocalTime.of(13, 45);
+        zaj[4] = LocalTime.of(15, 30);
+        zaj[5] = LocalTime.of(17, 15);
+
+        prz[0] = LocalTime.of(9, 45);
+        prz[1] = LocalTime.of(11, 30);
+        prz[2] = LocalTime.of(13, 15);
+        prz[3] = LocalTime.of(15, 15);
+        prz[4] = LocalTime.of(17, 00);
+        prz[5] = LocalTime.of(18, 45);
+        switch (dayOfWeek) {
+            case (1): {
+                return false;
+            }
+            case (7): {
+                return false;
+            }
+            default: {
+                if (now.getHour() > 19 || (now.getHour() == 18 && now.getMinute() >= 45)) {
+                    return false;
+                }
+
+                if (now.getHour() < 8 || (now.getHour() == 8 && now.getMinute() < 15)) {
+                    return false;
+                }
+            }
+
+            for (int i = 0; i < 6; i++) {
+                if (zaj[i].isBefore(now) && prz[i].isAfter(now)) {
+                    return true;
+                }
+
+                if (prz[i].isBefore(now) && zaj[i].isAfter(now)) {
+                    return false;
+                }
+
+                if (now.equals(zaj[i])) {
+                    return true;
+                }
+
+                if (now.equals(prz[i])) {
+                    return false;
+                }
+
+            }
+        }
+
+
+
+        return false;
+    };
+
+
+
+    static final void CheckActualStatus() {
+        LocalTime now = LocalTime.now();
+        long minutesBetween;
+        Date now1 = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(now1);
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+        boolean timestatus = checkBreak(dayOfWeek, now);
+        LocalTime midnight = LocalTime.MAX;
+        LocalTime[] zaj = new LocalTime[6];
+        LocalTime[] prz = new LocalTime[6];
+        zaj[0] = LocalTime.of(8, 15);
+        zaj[1] = LocalTime.of(10, 00);
+        zaj[2] = LocalTime.of(11, 45);
+        zaj[3] = LocalTime.of(13, 45);
+        zaj[4] = LocalTime.of(15, 30);
+        zaj[5] = LocalTime.of(17, 15);
+
+        prz[0] = LocalTime.of(9, 45);
+        prz[1] = LocalTime.of(11, 30);
+        prz[2] = LocalTime.of(13, 15);
+        prz[3] = LocalTime.of(15, 15);
+        prz[4] = LocalTime.of(17, 00);
+        prz[5] = LocalTime.of(18, 45);
+
+
+
+        switch (dayOfWeek) {
+            case (1): {
+                minutesBetween = ChronoUnit.MINUTES.between(now, midnight);
+                minutesBetween += 60 * 8 + 15;
+                PrintActualStatus(timestatus, minutesBetween);
+                return;
+            }
+            case (7): {
+                minutesBetween = ChronoUnit.MINUTES.between(now, midnight);
+                minutesBetween += 60 * 32 + 15;
+                PrintActualStatus(timestatus, minutesBetween);
+                return;
+            }
+            default: {
+                if (now.getHour() > 19 || (now.getHour() == 18 && now.getMinute() >= 45)) {
+                    minutesBetween = ChronoUnit.MINUTES.between(now, midnight);
+                    minutesBetween += 60 * 8 + 15;
+                    if (dayOfWeek == 6) {
+                        minutesBetween += 60 * 48;
+                    }
+                    PrintActualStatus(timestatus, minutesBetween);
+                    return;
+                }
+
+                if (now.getHour() < 8 || (now.getHour() == 8 && now.getMinute() < 15)) {
+                    minutesBetween = ChronoUnit.MINUTES.between(now, zaj[0]);
+                    PrintActualStatus(timestatus, minutesBetween);
+                    return;
+                }
+            }
+
+            for (int i = 0; i < 6; i++) {
+                if (zaj[i].isBefore(now) && prz[i].isAfter(now)) {
+                    minutesBetween = ChronoUnit.MINUTES.between(now, prz[i]);
+                    PrintActualStatus(timestatus, minutesBetween);
+                    return;
+                }
+
+                if (prz[i].isBefore(now) && zaj[i].isAfter(now)) {
+                    minutesBetween = ChronoUnit.MINUTES.between(now, zaj[i + 1]);
+                    PrintActualStatus(timestatus, minutesBetween);
+                    return;
+                }
+
+                if (now.equals(zaj[i])) {
+                    minutesBetween = ChronoUnit.MINUTES.between(now, prz[i]);
+                    PrintActualStatus(timestatus, minutesBetween);
+                    return;
+                }
+
+                if (now.equals(prz[i])) {
+                    minutesBetween = ChronoUnit.MINUTES.between(now, zaj[i + 1]);
+                    PrintActualStatus(timestatus, minutesBetween);
+                    return;
+                }
+
+            }
+        }
+    }
+
+
+    static final void PrintActualStatus(boolean timestatus, long minutes) {
+        minutes++;
+        if (timestatus == false) {
+            System.out.println("Zostalo " + minutes + " minut do konca przerwy.");
+        } else {
+            System.out.println("Zostalo " + minutes + " minut do konca zajec.");
+        }
+    }
+
 
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
 
+        try {
+            PrintWriter printfile = new PrintWriter("odp.txt");
+
+            quartz.scheduler.SimpleScheduler.Person k = new quartz.scheduler.SimpleScheduler.Person("", "", "", "");
+
+            HashBagMultimap<String, quartz.scheduler.SimpleScheduler.Person> persons = k.getPersons();
 
 
-        System.out.println("Executing like there is no tomorrow " + i);
+            SetIterable<String> keys;
+            keys = persons.keySet();
 
-        i++;
+            MutableList<String> list = keys.toList();
 
-        System.out.println(jobExecutionContext.getFireTime());
+            Collections.sort(list, String.CASE_INSENSITIVE_ORDER);
+            list.forEach(name -> {
+                printfile.println(name);
+                persons.get(name).forEach(personname -> {
+                    printfile.println("      " + personname.getPesel() + " " + personname.getName() + " " + personname.getSurname());
+                    //System.out.println(personname.getPesel() + " " + personname.getName() + " " + personname.getSurname());
+                });
+            });
+            printfile.close();
+/* 97102906482
+99051506146
+98060606476*/
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
 
+        if (LocalTime.now().getSecond() == 0) {
+            CheckActualStatus();
+
+        }
 
     }
+
+
 }
--- PracowaniaProgramowania/src/main/java/quartz/scheduler/SimpleScheduler.java
+++ PracowaniaProgramowania/src/main/java/quartz/scheduler/SimpleScheduler.java
@@ -1,48 +1,138 @@
 package quartz.scheduler;
-
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.Trigger;
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.api.set.SetIterable;
+import org.eclipse.collections.impl.multimap.bag.HashBagMultimap;
+import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 import quartz.job.SimpleJob;
 
+import java.util.List;
+import java.util.Scanner;
+
 import static org.quartz.CronScheduleBuilder.cronSchedule;
 import static org.quartz.JobBuilder.newJob;
-import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+
 import static org.quartz.TriggerBuilder.newTrigger;
 
+
+
 public class SimpleScheduler {
+
+
+    static public final boolean peselcontrol(String pesel) {
+
+        int[] k = new int[11];
+        int sum = 0;
+        if (pesel.length() != 11) {
+            System.err.println("Error - pesel is too short or too long!");
+            return false;
+        }
+        for (int i = 0; i < pesel.length(); i++) {
+            k[i] = (Character.getNumericValue(pesel.charAt(i)));
+            if (k[i] > 9) {
+                System.err.println("Error - pesel must not contain letters.");
+                return false;
+            }
+        }
+        sum = (9 * k[0] + 7 * k[1] + 3 * k[2] + k[3] + 9 * k[4] + 7 * k[5] + 3 * k[6] + k[7] + 9 * k[8] + 7 * k[9]) % 10;
+        if (k[10] != sum) {
+            System.err.println("Error - pesel is wrong.");
+            return false;
+        }
+        return true;
+    }
+
+    ;
+
+    public static class Person {
+        String name, surname, pesel, city;
+        static HashBagMultimap<String, Person> persons;
+
+
+        public Person(String name, String surname, String pesel, String city) {
+            this.name = name;
+            this.surname = surname;
+            this.pesel = pesel;
+            this.city = city;
+        }
+
+        public static HashBagMultimap<String, Person> getPersons() {
+            return persons;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getSurname() {
+            return surname;
+        }
+
+        public String getPesel() {
+            return pesel;
+        }
+    }
+
+    ;
+
+
 
     public static void main(String[] args) throws InterruptedException {
 
         try {
-            // Grab the Scheduler instance from the Factory
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            Person newperson;
+            List<Person> personList = null;
+            SetIterable<String> keys;
+            MutableList<String> cityList = null;
+            String city, nam, surname, pesel;//w nim zapiszemy swoje imie
+            Scanner inputscan; //obiekt do odebrania danych od użytkownika
 
-            // define the job and tie it to our SimpleJob class
+
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             JobDetail job = newJob(SimpleJob.class)
                     .withIdentity("job1", "group1")
                     .build();
-
-            // Trigger the job to run now, and then repeat every 1 seconds
             Trigger trigger = newTrigger()
                     .withIdentity("trigger1", "group1")
                     .startNow()
-                    .withSchedule(cronSchedule("0/1 * * * * ?"))
+                    .withSchedule(cronSchedule("0/30 * * ? * * *"))
                     .build();
 
-
-            // Tell quartz to schedule the job using our trigger
             scheduler.scheduleJob(job, trigger);
 
-            // and start it off
             scheduler.start();
 
-            // Sleep for 6 seconds and then shutdown the scheduler
-            Thread.sleep(6000);
+            newperson = new Person("","","","");
+            newperson.persons = HashBagMultimap.newMultimap();
 
-            scheduler.shutdown();
+            while (true) {
+                inputscan = new Scanner(System.in);
+                city = inputscan.nextLine();
+                nam = inputscan.next();
+                surname = inputscan.next();
+                pesel = inputscan.next();
+
+                if (peselcontrol(pesel) == true) {
+                    newperson = new Person(nam, surname, pesel, city);
+                    keys = newperson.persons.keySet();
+                    cityList = keys.toList();
+                    personList = newperson.persons.valuesView().toList();
+                    for (int i = 0; i < personList.size(); i++) {
+                        String k = personList.get(i).getPesel();
+
+                        if (pesel.equals(k)) {
+                            for (int j = 0; j < cityList.size(); j++) {
+                                newperson.persons.remove(cityList.get(j), personList.get(i));
+                            }
+                        }
+                    }
+                    newperson.persons.put(city, newperson);
+
+
+                }
+
+            }
+
 
         } catch (SchedulerException se) {
             se.printStackTrace();
